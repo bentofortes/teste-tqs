865a0e09ad472223cceb8e1881fbd0509e13e1f6
bentofortes <bentomaranhao@gmail.com>
2026-04-29 22:22:42 -0300

    add npx to run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 
-echo Installing frontend dependencies...
-start cmd /k "cd frontend && npm install && ng serve --open"
+echo Starting frontend...
+start cmd /k "cd frontend && npm install && npx ng serve --open"
 
 echo Starting backend...
 start cmd /k "cd backend && dotnet restore && dotnet run"
